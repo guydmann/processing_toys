400a254b2ebfc3cb8d0df700f787ee2aa4c6dc20
Guy Mann <guydmann@gmail.com>
2010-09-07 22:55:57 -0400

    did a bunch of work to figure out how to make my processing applications accessible from html, turns out i was calling them wrong which was caused by my additions to processing, so i fixed the bugs i created and now i have access from javascript in the page

--- clock.php
+++ clock.php
@@ -8,7 +8,7 @@
 <?php include("../../topbar.phtml"); ?>
 <script type="text/javascript" src="./processing-0.9.4.js"></script>
 <script type="text/javascript" src="./processing.init.js"></script>
-
+<script type="text/javascript" src="./clock_control.js"></script>
 
 
 <div style="padding: 3em">
@@ -20,12 +20,19 @@ suggest using Chrome<br>
 This clock has been submitted to the <a href="http://www.tokyoflash.com/blog/?p=1725&lang=en">Tokyo Flash Design Contest</a>
 <br>
 <br>
-To enlarge a small clock merely click on it<br>
+To enlarge a small clock click on the image or any of the links below<br>
+<a href="javascript:setClock(0)">Clock 0</a>
+&nbsp;&nbsp;
+<a href="javascript:setClock(1)">Clock 1</a>
+&nbsp;&nbsp;
+<a href="javascript:setClock(2)">Clock 2</a>
+&nbsp;&nbsp;
+<a href="javascript:setClock(3)">Clock 3</a>
+&nbsp;&nbsp;
+<a href="javascript:setClock(4)">Clock 4</a>
 <br>
-<script type="application/processing">
-<?php include("./clock.pjs"); ?>
-</script>
-<canvas width="400" height="400"></canvas>
+<br>
+<canvas id="clock" data-src="clock.pjs" width="400" height="400"></canvas>
 </div>
 </body>
 </html>
