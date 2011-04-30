4fb3d5a02c566bd918f908697695487bea31be68
Guy Mann <guydmann@gmail.com>
2011-04-29 20:17:04 -0400

    title change

--- clock.php
+++ clock.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
-<?php include("../../header.html"); ?>
+<?php 
+if (!isset($_GET['clock']) || $_GET['clock'] <= "") { 
+	$center_script = "";
+} else {
+	$center_script = " onLoad='setClock(" . $_GET['clock'] . ")'";
+}
 
-<body>
+include("../../header.html"); ?>
+
+<body <?php echo $center_script;?>>
 
 <?php include("../../topbar.phtml"); ?>
 <script type="text/javascript" src="./processing-0.9.4.js"></script>
@@ -12,14 +19,15 @@
 
 
 <div style="padding: 3em">
-<h2>Clock App</h2>
+<h2>Hypno Clock</h2>
 runs smoothly in Chrome<br>
-Firefox image is crisper<br>
+In Firefox image is crisper<br>
 suggest using Chrome<br>
 <br>
 This clock has been submitted to the <a href="http://www.tokyoflash.com/blog/?p=1725&lang=en">Tokyo Flash Design Contest</a>
-<br>
-<br>
+<br><br>
+The git repository is <a href="http://github.com/guydmann/processing_toys">here</a>
+<br><br>
 To enlarge a small clock click on the image or any of the links below<br>
 <a href="javascript:setClock(0)">Clock 0</a>
 &nbsp;&nbsp;
